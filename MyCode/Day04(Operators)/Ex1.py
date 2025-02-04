f5cf8ad86f0818e76bb24de6c555836e92a36ca1
Vyjayanthi1 <vyjayanthipeddireddy@gmail.com>
2025-02-04 12:25:00 +0530

    operators

---
+++ MyCode/Day04(Operators)/Ex1.py
@@ -0,0 +1,29 @@
+# Python Operators:
+# There are seven types of python operators
+
+# 1. Arthematic Operators
+# 2. Assignment Operators
+# 3. Logical Operators
+# 4. Comparision/Relational Operator
+# 5. Identity Operator
+# 6.Bitwise Operator
+
+# 1. Arthematic operator:
+# (A
+a = 12
+b = 10
+print('addition of a + b :',a + b)
+print('difference of a - b: ', a - b )
+print('product of a * b : ', a * b)
+print('division of a / b :',a/b)
+print('floor division of a // b:', a//b)
+print('moduls of a % b :',a % b)
+print('exponentiation a**b :',a**b)
+
+# addition of a + b : 22
+# difference of a - b:  2
+# product of a * b :  120
+# division of a / b : 1.2
+# floor division of a // b: 1
+# moduls of a % b : 2
+# exponentiation a**b : 61917364224
